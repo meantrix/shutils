e64d168026d02977accc0e0519a680e120c86e71
Igor-Siciliani <36808532+igor-siciliani@users.noreply.github.com>
2023-06-01 15:32:09 -0300

    refact shutils main function with list method

--- shutils-0.2/help.sh
+++ shutils-0.2/help.sh
@@ -8,12 +8,17 @@
 #######################################
 help () {
 
+    source "/bin/shutils/list.sh"
+    local funcs=($(list))
+
 	local func="$1"
     local file="/bin/shutils/$func.sh"		
 	# Extrair a documentação da função do arquivo
         local doc=$(sed -n -e '/^#######################################/,/^#######################################/p' "$file")
     if [ -z "$doc" ]; then
         echo "help: Invalid function: $1"
+        echo "Available methods:"
+        printf "%s\n" "${funcs[@]}"
     else
         echo "Use: shutils help $func"
         echo
@@ -24,3 +29,5 @@ help () {
         echo "help."
     fi
 }
+
+
