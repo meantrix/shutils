cceaa6c753d585fe26d6059a47e0ccc68422446a
Igor-Siciliani <36808532+igor-siciliani@users.noreply.github.com>
2023-06-01 15:31:39 -0300

    create list method

---
+++ shutils-0.2/list.sh
@@ -0,0 +1,12 @@
+#######################################
+# List available methods in shutils
+# Arguments: None
+# Returns: None
+#######################################
+list() {
+    # Define the available methods
+    local funcs=("azcopy10inst" "dockerhubp" "filefirst" "filecompress" "filenum" "filextract" "filelarlist" "gitcommit" "gitinfo" "gitinfall" "kp" "jsonload" "myinfo" "netinfo" "netmyip" "podel" "podget" "podlog" "podvalidname" "podres" "psa" "ssd" "whoisport" "nviuse" "version" "help" "list")
+
+    # Print the list of available methods
+    echo "${funcs[@]}"
+}
